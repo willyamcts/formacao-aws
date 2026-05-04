5d5765af21b4c0f936157e1f9c4cea5acbdf9580
Willyam Castro <willyam.android@gmail.com>
2026-05-03 22:28:53 -0300

    to populate postgres database from a CSV file

---
+++ scripts/database/postgres-import_csv.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Variáveis de conexão
+DB_HOST="RDS_ENDPOINT"
+DB_PORT="5432"
+DB_NAME="postgres"
+DB_USER='USERNAME'
+DB_PASSWORD='SENHA'
+
+# Comando psql para se conectar e executar uma consulta SQL
+PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" -U "$DB_USER" -c "\COPY public.\"Tarefas\" FROM 'tarefas.csv' DELIMITER ',' CSV"

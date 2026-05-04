cdeca289c377517830a5c8b6061bb7d91defad80
Willyam Castro <willyam.android@gmail.com>
2026-05-03 23:15:25 -0300

    added break lines

--- scripts/database/postgres-import_csv.sh
+++ scripts/database/postgres-import_csv.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 # Variáveis de conexão
-DB_HOST="RDS_ENDPOINT"
+DB_HOST="bia-aurora.cluster-cqzmsio0yzwj.us-east-1.rds.amazonaws.com"
 DB_PORT="5432"
-DB_NAME="postgres"
-DB_USER='USERNAME'
+DB_NAME="bia"
+DB_USER='postgres'
 DB_PASSWORD='SENHA'
 
 # Comando psql para se conectar e executar uma consulta SQL
-PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" -U "$DB_USER" -c "\COPY public.\"Tarefas\" FROM 'tarefas.csv' DELIMITER ',' CSV"
+for i in $(ls data-*.csv); do
+  echo "Importando arquivo $i"
+  PASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" \
+    -p "$DB_PORT" \
+    -d "$DB_NAME" -U "$DB_USER" -c "\COPY public.\"Tarefas\" FROM "data-${i}.csv" DELIMITER ',' CSV"
+done
